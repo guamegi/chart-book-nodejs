3c574dd44577359b4f0a6265fd20c683ad210e18
Minha Kwon <namisoft@naver.com>
2023-02-27 22:25:16 +0900

    불필요한 파일 제거

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,9 +1,33 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+// import React from "react";
+// import { render } from "@testing-library/react";
+// import App from "./App";
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+// test('renders learn react link', () => {
+//   const { getByText } = render(<App />);
+//   const linkElement = getByText(/learn react/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
+
+function getUser(id) {
+  return {
+    id,
+    email: `user${id}@test.com`,
+  };
+}
+
+test("1 is 1", () => {
+  expect(1).toBe(1);
+});
+test("return a user object", () => {
+  expect(getUser(1)).toStrictEqual({ id: 1, email: "user1@test.com" });
+});
+test("number 0 is falsy but string 0 is truthy", () => {
+  expect(0).toBeFalsy();
+  expect("0").toBeTruthy();
+});
+test("array", () => {
+  const colors = ["red", "yellow", "blue"];
+  expect(colors).toHaveLength(3);
+  expect(colors).toContain("red");
+  expect(colors).not.toContain("redd");
 });
--- client/src/index.js
+++ client/src/index.js
@@ -3,11 +3,9 @@ import ReactDOM from "react-dom";
 import "./assets/bootstrap/css/bootstrap.min.css";
 import "./assets/fonts/fontawesome-all.min.css";
 import "./assets/css/theme.css";
-// import $ from 'jquery';
 import "./assets/bootstrap/js/bootstrap.min.js";
 import "./assets/js/theme.js";
 import App from "./App";
-import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(
   <React.StrictMode>
@@ -15,8 +13,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById("root")
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
