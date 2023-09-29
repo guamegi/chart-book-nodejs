ab849b4585451e246d33f1bceab4588018f4439a
Minha Kwon <namisoft@naver.com>
2023-09-29 12:09:20 +0900

    코드 순서 변경

--- server.js
+++ server.js
@@ -4,6 +4,23 @@ const app = express();
 const path = require("path");
 const { createProxyMiddleware } = require("http-proxy-middleware");
 
+const whitelist = [
+  "https://web-chart-book-client-jvpb2aln39jnz0.sel5.cloudtype.app",
+];
+
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      // 만일 whitelist 배열에 origin인자가 있을 경우
+      callback(null, true); // cors 허용
+    } else {
+      callback(new Error("Not Allowed Origin!")); // cors 비허용
+    }
+  },
+};
+
+app.use(cors(corsOptions)); // 옵션을 추가한 CORS 미들웨어 추가
+
 // 주식 종목 시세 호출
 app.use(
   createProxyMiddleware("/api", {
@@ -27,23 +44,6 @@ app.use(
     changeOrigin: true,
   })
 );
-
-const whitelist = [
-  "https://web-chart-book-client-jvpb2aln39jnz0.sel5.cloudtype.app",
-];
-
-const corsOptions = {
-  origin: function (origin, callback) {
-    if (whitelist.indexOf(origin) !== -1) {
-      // 만일 whitelist 배열에 origin인자가 있을 경우
-      callback(null, true); // cors 허용
-    } else {
-      callback(new Error("Not Allowed Origin!")); // cors 비허용
-    }
-  },
-};
-
-app.use(cors(corsOptions)); // 옵션을 추가한 CORS 미들웨어 추가
 
 // 미들웨어 함수를 특정 경로에 등록
 // app.use("/api/data", function (req, res) {
